8d702d653af91c96ee028fd63f3d8e62b91747a3
Kohei Iwasaki <jupiter@sophia.ac.jp>
2013-09-25 11:29:11 +0900

    actor verb object

---
+++ models/statements.js
@@ -0,0 +1,32 @@
+var mongo = require('mongoskin');
+var db = mongo.db('localhost:27017/tamuralrs');
+var statements = db.collection('statements');
+
+exports.pull = function pull(page, cb){
+	var p = {};
+	var rowsPer = 10;
+	var skip;
+	var errs;
+
+	page = page || 1;
+	skip = (page - 1) * rowsPer;
+
+	statements.findEach({}, {limit: rowsPer, skip: skip }, function(err, doc){
+		if(err){ errs = errs || []; errs.push(err); }
+		if(doc){
+			p[doc._id] = doc;
+			delete p[doc._id]._id;
+			return;
+		}
+		cb(errs, p);
+	});
+};
+
+exports.del = function(statement, cb){
+	statements.removeById(statement, cb);
+};
+
+exports.add = function(statement, cb){
+	console.log('statement:'+JSON.stringify(statement));
+	statements.insert(statement.state,cb);
+};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,23 @@
  * GET home page.
  */
 
+var statements = require('../models/statements');
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	statements.pull(req.param.pagenum,function(err,statements){
+		if(err) console.log(err);
+		console.log(JSON.stringify(statements));
+		res.render('index', {
+			title: 'TamuraLRS',
+			statements: statements,
+			page: req.params.pagenum
+		});
+	});
 };
+
+exports.state = function(req, res){
+	statements.add(req.body, function(err){
+		if(err){ console.log(err); }
+		res.redirect(req.header('Referrer') || '/');
+	});
+}
