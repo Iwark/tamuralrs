03fea536e4b18adbc573ad048a413fa21fb8fd50
Kohei Iwasaki <jupiter@sophia.ac.jp>
2013-10-03 13:16:56 +0900

    state

--- app.js
+++ app.js
@@ -20,10 +20,16 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.all('/state', function(req, res, next) {
+app.use('/state', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+  res.header("Access-Control-Allow-Headers", "Content-Type, application/x-www-form-urlencoded");
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
 });
 app.use(express.static(path.join(__dirname, 'public')));
 
