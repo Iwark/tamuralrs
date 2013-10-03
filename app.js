797c9119d33f25260e14b2fdc43ed42b91a9352b
Kohei Iwasaki <jupiter@sophia.ac.jp>
2013-10-03 13:11:51 +0900

    pos

--- app.js
+++ app.js
@@ -15,16 +15,16 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.all('/state', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+app.all('/state', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
