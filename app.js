ada4907c70341cd092bcd1b1ef8131ebd69e61aa
Kohei Iwasaki <jupiter@sophia.ac.jp>
2013-10-03 13:38:31 +0900

    set

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use('/state', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "X-Requested-With");
+  res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.set("Access-Control-Allow-Headers", "Content-Type");
   if ('OPTIONS' == req.method) {
     res.send(200);
   }
